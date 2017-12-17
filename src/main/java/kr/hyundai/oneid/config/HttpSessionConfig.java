e626978bd5f16e0cbafa1ea80abea9ea7004266d
eugenekim <littlehome.eugene@gmail.com>
2017-12-17 15:24:00 +0900

    domain

--- src/main/java/kr/hyundai/oneid/config/HttpSessionConfig.java
+++ src/main/java/kr/hyundai/oneid/config/HttpSessionConfig.java
@@ -16,31 +16,25 @@
 
 package kr.hyundai.oneid.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 // tag::class[]
 @EnableJdbcHttpSession // <1>
 public class HttpSessionConfig {
 
-	// @Bean
-	// public EmbeddedDatabase dataSource() {
-	// 	return new EmbeddedDatabaseBuilder() // <2>
-	// 			.setType(EmbeddedDatabaseType.H2)
-	// 			.addScript("org/springframework/session/jdbc/schema-h2.sql").build();
-	// }
-
-	// @Bean
-	// public PlatformTransactionManager transactionManager(DataSource dataSource) {
-	// 	return new DataSourceTransactionManager(dataSource); // <3>
-	// }
+	// tag::cookie-serializer[]
+	@Bean
+	public CookieSerializer cookieSerializer() {
+		DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+		// serializer.setCookieName("JSESSIONID"); // <1>
+		// serializer.setCookiePath("/"); // <2>
+		serializer.setDomainNamePattern("^\\..+?\\.(\\w+\\.[a-z]+)$"); // <3>
+		return serializer;
+	}
+	// end::cookie-serializer[]
 
 }
 // end::class[]
