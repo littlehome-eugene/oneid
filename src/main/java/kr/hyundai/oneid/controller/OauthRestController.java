61a76c5d29821db6b0c29b63d5e38b006dd1664b
eugenekim <littlehome.eugene@gmail.com>
2017-12-20 15:56:40 +0900

    f

--- src/main/java/kr/hyundai/oneid/controller/OauthRestController.java
+++ src/main/java/kr/hyundai/oneid/controller/OauthRestController.java
@@ -1,21 +1,27 @@
 package kr.hyundai.oneid.controller;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.hyundai.oneid.model.oauth.Client;
+import kr.hyundai.oneid.service.OauthService;
 
 
 @RestController
 public class OauthRestController {
 
-  @RequestMapping("/client/register", method=RequestMethod.POST)
-  public String clientRegister(Client client) {
+  @Autowired
+  OauthService service;
+
+  @RequestMapping("/client/register")
+  public String clientRegister(String redirectUri1) {
 
 		BaseClientDetails clientDetails = new BaseClientDetails();
     String clientId = UUID.randomUUID().toString();
@@ -23,16 +29,15 @@ public class OauthRestController {
     String clientSecret = UUID.randomUUID().toString();
 		clientDetails.setClientSecret(clientSecret);
 
-    clientDetails.setScope("read");
     List<String> scopes = Arrays.asList("read");
+    clientDetails.setScope(Arrays.asList("read"));
     clientDetails.setAutoApproveScopes(scopes);
 
     List<String> grantTypes = Arrays.asList("authorization-code");
     clientDetails.setAuthorizedGrantTypes(grantTypes);
 
-    String redirectUri = client.getRedirectUri1();
-    List<String> redirectUris = Arrays.asList(redirectUri);
-    clientDetails.setRedirectUris(redirectUris);
+    Set<String> redirectUris = new HashSet<String>(Arrays.asList(redirectUri1));
+    clientDetails.setRegisteredRedirectUri(redirectUris);
 
 		service.addClientDetails(clientDetails);
     return clientDetails;
